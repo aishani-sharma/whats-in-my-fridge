103c60aed440260b022fdc7ace0d34e36b6415ee
aishani-sharma <aishanisharma3@gmail.com>
2026-06-08 21:28:27 +0530

    Updated Image Selection

--- lib/screens/ingredient_screen.dart
+++ lib/screens/ingredient_screen.dart
@@ -174,7 +174,7 @@ class _IngredientScreenState extends State<IngredientScreen> {
                             child: Container(
                               decoration: BoxDecoration(
                                 color: const Color(0xFFE8748A)
-                                    .withOpacity(0.15),
+                                    .withValues(alpha: 0.15),
                                 border: Border.all(
                                   color: const Color(0xFFE8748A),
                                   width: 2,
